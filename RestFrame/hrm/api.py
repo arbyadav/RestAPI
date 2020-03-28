c9aa6f9c98dbaaedf8a4e4e71acfbcf19eadff23
Yadav <52038474@acsind.com>
2020-03-28 17:13:59 +0530

    final commit

--- RestFrame/hrm/api.py
+++ RestFrame/hrm/api.py
@@ -1,3 +1,4 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,59 +9,62 @@ from rest_framework.authtoken.models import Token
 
 
 class UserAuthentication(ObtainAuthToken):
-    def post(self,request,*args,**kwargs):
-        serial=self.serializer_class(data=request.data,context={'request':request})
+    def post(self, request, *args, **kwargs):
+        serial = self.serializer_class(
+            data=request.data, context={'request': request})
         serial.is_valid(raise_exception=True)
-        user=serial.validated_data['user']
-        token, created=Token.objects.get_or_create(user=user)
+        user = serial.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
         return Response(token.key)
+
 
 class UserDList(APIView):
 
-        def get(self,request):
-            model=UsersD.objects.all();
-            serial=UsersDSerial(model,many=True)
-            return Response(serial.data)
-        
-        def post(self,request):
-            serial=UsersDSerial(data=request.data)
-            if(serial.is_valid()):
-                serial.save()
-                return Response(serial.data,status=status.HTTP_201_CREATED)
-            else:
-                return Response(serial.errors,status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request):
+        model = UsersD.objects.all()
+        serial = UsersDSerial(model, many=True)
+        return Response(serial.data)
+
+    def post(self, request):
+        serial = UsersDSerial(data=request.data)
+        if(serial.is_valid()):
+            serial.save()
+            return Response(serial.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserDetail(APIView):
 
-    def getuser(self,employee_id):
+    def getuser(self, employee_id):
         try:
-            model=UsersD.objects.get(id=employee_id)
+            model = UsersD.objects.get(id=employee_id)
             return model
         except UsersD.DoesNotExist:
-            return 
-    
-    def get(self,request,employee_id): 
-        if not self.getuser(employee_id):
-            return Response(f'User with ID {employee_id} is not found in database',status=status.HTTP_404_NOT_FOUND)  
+            return
 
-        serial=UsersDSerial(self.getuser(employee_id))
+    def get(self, request, employee_id):
+        if not self.getuser(employee_id):
+            return Response(f'User with ID {employee_id} is not found in database', status=status.HTTP_404_NOT_FOUND)
+
+        serial = UsersDSerial(self.getuser(employee_id))
         return Response(serial.data)
-        
-    def put(self,request,employee_id):
-        if not self.getuser(employee_id):
-            return Response(f'User with ID {employee_id} is not found in database',status=status.HTTP_404_NOT_FOUND)  
 
-        serial=UsersDSerial(self.getuser(employee_id),data=request.data)
+    def put(self, request, employee_id):
+        if not self.getuser(employee_id):
+            return Response(f'User with ID {employee_id} is not found in database', status=status.HTTP_404_NOT_FOUND)
+
+        serial = UsersDSerial(self.getuser(employee_id), data=request.data)
         if(serial.is_valid()):
             serial.save()
-            return Response(serial.data,status=status.HTTP_201_CREATED)
+            return Response(serial.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serial.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self,request,employee_id):
+    def delete(self, request, employee_id):
         if not self.getuser(employee_id):
-            return Response(f'User with ID {employee_id} is not found in database',status=status.HTTP_404_NOT_FOUND)  
-            
-        model=self.getuser(employee_id)
+            return Response(f'User with ID {employee_id} is not found in database', status=status.HTTP_404_NOT_FOUND)
+
+        model = self.getuser(employee_id)
         model.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
